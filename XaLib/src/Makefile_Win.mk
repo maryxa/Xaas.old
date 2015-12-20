6531bbe63808e64c80855bd24f07b66d725fba29
Alessandro Mariotti <alex@xallegro.com>
2015-12-20 19:19:33 +0000

    Added include for OS

--- XaLib/src/Makefile_Win.mk
+++ XaLib/src/Makefile_Win.mk
@@ -3,27 +3,23 @@
 ##  Alessandro Mariotti
 ##  alex@xallegro.com
 
-
-## WINDOWS MINGW32_NT
-## TESTED: MINGW32_NT-6.2
-## TESTED WIN7 - WIN10
 ## ASSUMING XAAS INSTALLED UNDER c:/XAllegro
 
-    MYSQL_LIB = C:\XAllegro\libs\mysql
-    MINGW_LIB_DIR=C:\XAllegro\msys\1.0\opt\windows_32\include
+MYSQL_LIB = C:\XAllegro\libs\mysql-connector-c-6.1.6-winx64
+MINGW_LIB_DIR=C:\XAllegro\msys32\mingw64\include
 
-    CPP      = g++
-    OBJDIR   = ..\objects\\
-    RM       = rm -f -r
-    CUR_DIR  = $(cmd %cd%)
+CPP      = g++
+OBJDIR   = ..\objects\\
+RM       = rm -f -r
+CUR_DIR  = $(cmd %cd%)
 
-    LIBS     = -static -L$(MYSQL_LIB)\lib 
-    CPPINCS  = -I..\include -I..\include -I$(MINGW_LIB_DIR) \
-		-I$(MYSQL_LIB)\include -I$(MINGW_LIB_DIR)\libxml2 \
+LIBS     = -static -L$(MYSQL_LIB)\lib 
+CPPINCS  = -I..\include -I..\include -I$(MINGW_LIB_DIR) \
+	   -I$(MYSQL_LIB)\include -I$(MINGW_LIB_DIR)\libxml2 \
 
-    CPPFLAGS = $(CPPINCS) -std=c++0x -O3 -D__NO_INLINE__ -Wall
+CPPFLAGS = $(CPPINCS) -std=c++14 -O3
 
-    BIN      = ..\bin\XaLib.a
+BIN      = ..\bin\XaLib.a
 
 .KEEP_STATE:
 .KEEP_STATE_FILE:.make.state.GNU-amd64-Windows
